0d2afff012362da0dd748d079e57108b9b88d485
Lukas Szopo <lukas.szopo@denzelbank.at>
2024-02-19 09:49:28 +0100

    Correction call PS5.1

--- Export-NavObjects/Export-NavObjects.ps1
+++ Export-NavObjects/Export-NavObjects.ps1
@@ -1,1 +1,74 @@
+#Requires -Version 5.1
+
+param (
+    # Specifies the computer name where the sql database resides
+    [Parameter(Mandatory)]
+    [string]
+    $DataSource,
+    # Specifies the database name of the
+    [Parameter(Mandatory)]
+    [string]
+    $DatabaseName,
+    # Specifies a path to store the objects. If a relative path is specified, $PSScriptRoot will be used as root
+    [Parameter()]
+    [string]
+    $Path,
+    # Specifies a filter on the version list of the objects to export
+    [Parameter()]
+    [string]
+    $ObjectFilter,
+    # Specifies if file explorer should be openend after exporting the objects
+    [Parameter()]
+    [switch]
+    $RevealInFileExplorer
+    
+    )
+    # NAV cmdlets are only compatible with powershell version 5.1
+    if ($PSVersionTable.PSEdition -ne 'Desktop') {
+    # Re-launch as version 5 if we're not already
+    if ($RevealInFileExplorer) {
+        powershell -Version 5.1 -File $MyInvocation.MyCommand.Definition -DataSource $dataSource -DatabaseName $databaseName -Path $path -ObjectFilter $ObjectFilter -RevealInFileExplorer
+    } else {
+        powershell -Version 5.1 -File $MyInvocation.MyCommand.Definition -DataSource $dataSource -DatabaseName $databaseName -Path $path -ObjectFilter $ObjectFilter 
+    }
+
+    exit
+}
 Set-StrictMode -Version 3.0
+$env:PSModulePath = $env:PSModulePath.Replace(";$HOME\Documents\PowerShell\Modules", '')
+$env:PSModulePath += ";$HOME\Documents\PowerShell\Modules"
+
+if (-not (Get-Module PsLibNavTools)) {
+    Import-Module PsLibNavTools
+}
+if (-not (Get-Module PsLibPowerShellTools)) {
+    Import-Module PsLibPowerShellTools
+}
+
+function main {
+    $DestinationDirectory
+    
+    if ([System.IO.Path]::IsPathRooted($Path)) {
+        $DestinationDirectory = $Path
+    } else {
+        $TempFolder = New-TemporaryDirectory
+        $DestinationDirectory = Join-Path -Path $TempFolder $Path
+    }
+
+    Write-Verbose 'Exporting objects'
+    $tempFile = [IO.Path]::GetTempFileName() | Rename-Item -NewName { $_ -replace 'tmp$', 'txt' } -PassThru
+    Export-NavApplicationObjectsAsTxt -dataSource $dataSource -databaseName $databaseName -exportFile $tempFile -filter $ObjectFilter
+
+    Write-Verbose 'Splitting objects'
+    Split-NavApplicationObjectsTxt -sourceFile $tempFile -destinationDirectory $DestinationDirectory
+
+    Remove-Item $tempFile
+
+    if ($revealInFileExplorer) {
+        Set-Location $DestinationDirectory
+        Invoke-Item .
+    }
+    return $TempFolder
+}
+
+main
--- Export-NavObjects/Export-NavObjectsInteractive.ps1
+++ Export-NavObjects/Export-NavObjectsInteractive.ps1
@@ -1,22 +1,37 @@
-Set-StrictMode -Version 3.0
-
 if (-not (Get-Module PsLibConfigurationManager)) {
     Import-Module PsLibConfigurationManager
 }
+
+$config = Get-Configuration -configurationFile "$PSScriptRoot\config\Export-NavObjects_Config.json"
 
 Write-Host 'Aus welcher Datenbank sollen Objekte exportiert werden?'
 $i = 0
 foreach ($SourceSystem in $config.SourceSystem) {
     $i += 1
-    Write-Host $i ': ' $SourceDisplayName
+    Write-Host $i ': ' $SourceSystem.DisplayName
 }
 Write-Host 'Strg-C : Abbruch'
+
+$choice = Read-Host '> '
+if ($choice -notin 1..$i) {
+    throw "Ungueltige Auswahl: $choice. Abbruch."
+}
+$srcSystem = $config.SourceSystem[$choice - 1]
+
 
 Write-Host ' '
 Write-Host 'Welche Objekte sollen exportiert werden?'
 $i = 0
 foreach ($ObjectGroup in $config.ObjectGroup) {
     $i += 1
-    Write-Host $i ': ' $ObjectGroupDisplayName
+    Write-Host $i ': ' $ObjectGroup.DisplayName
 }
+Write-Host 'Strg-C : Abbruch'
 
+$choice = Read-Host '> '
+if ($choice -notin 1..$i) {
+    throw "Ungueltige Auswahl: $choice. Abbruch."
+}
+$objGrp = $config.ObjectGroup[$choice - 1]
+
+.\Export-NavObjects\Export-NavObjects.ps1 -DataSource $srcSystem.DataSource -DatabaseName $srcSystem.DatabaseName -Path $objGrp.Path -ObjectFilter $objGrp.ObjectFilter -RevealInFileExplorer
